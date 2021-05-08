35619cae6aaf18105381a42f284ce6f4d135e39c
8bitgeek <mike@8bitgeek.net>
2021-05-08 15:07:37 -0400

    clock demo

--- ports/stm32/boards/AFX-102-REVB-BRISCITS/test/asyncio_clock.py
+++ ports/stm32/boards/AFX-102-REVB-BRISCITS/test/asyncio_clock.py
@@ -1,19 +1,26 @@
 import uasyncio as asyncio
 import microamp as amp
+import gc
+
+def callback():
+    print("callback")
 
 async def clock():
     if amp.endpoint_indexof("clock") >= 0:
         print(amp.endpoint_count())
         fd=amp.channel_open("clock")
         if fd >= 0:
+            amp.channel_callback(fd,callback)
             while True:
                 avail = amp.channel_avail(fd)
                 if avail > 0:
                     buf=b'\x00\x00\x00\x00'
                     if amp.channel_read(fd,buf,4) >= 0:
-                        b = list(buf)
-                        print(b[0] | b[1]<<8 | b[2]<<16 | b[3]<<24)
-                await asyncio.sleep_ms(1)
+                        data = list(buf)
+                        tm = data[0] | data[1]<<8 | data[2]<<16 | data[3]<<24
+                        print(tm,gc.mem_alloc())
+                        #gc.collect()
+                await asyncio.sleep_us(1)
 
 
 asyncio.run(clock())
