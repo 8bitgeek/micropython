81fbf7f95bee840b6cfe7178e3f69a6b445b3f7e
8bitgeek <mike@8bitgeek.net>
2021-05-08 20:19:12 -0400

    dataready handler

--- ports/stm32/boards/AFX-102-REVB-BRISCITS/test/callback_clock.py
+++ ports/stm32/boards/AFX-102-REVB-BRISCITS/test/callback_clock.py
@@ -3,24 +3,23 @@ import microamp as amp
 import gc
 
 # the rx data callback handler
-def rx_event(fd):
+def dataready_handler(fd):
     avail = amp.channel_avail(fd)
     buf=b'\x00\x00\x00\x00\x00\x00'
     if amp.channel_read(fd,buf,4) >= 0:
         data = list(buf)
         tm = data[0] | data[1]<<8 | data[2]<<16 | data[3]<<24
-        print("rx_event",tm)
-        gc.collect()
+        print("dataready_handler",tm)
 
-# open the ondpoint and assign a callback function.
+# open the endpoint and assign a event callback function.
 if amp.endpoint_indexof("clock") >= 0:
     fd=amp.channel_open("clock")
     if fd >= 0:
-        amp.channel_callback(fd,rx_event,fd)
+        amp.channel_dataready_handler(fd,dataready_handler,fd)
 
 # just doing some busy work here.
 async def busy():
     while True:
-        await asyncio.sleep_ms(2000)
+        await asyncio.sleep_ms(100)
 
 asyncio.run(busy())
