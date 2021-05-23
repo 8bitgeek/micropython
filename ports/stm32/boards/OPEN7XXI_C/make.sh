9646a00312942949d36c96003e11ca5a04af6416
8bitgeek <mike@8bitgeek.net>
2021-05-23 11:55:18 -0400

    Open7xx Olimex JTAG Support Scripts

--- ports/stm32/boards/OPEN7XXI_C/make.sh
+++ ports/stm32/boards/OPEN7XXI_C/make.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+BOARD=OPEN7XXI_C
+OPENOCD_DIR=boards/$BOARD/openocd
+OPENOCDCONFIG=$OPENOCD_DIR/openocd-$JTAG.cfg
+
 if [ "$1" == "start" ]; then
 
-    openocd -f interface/stlink-v2.cfg -f target/stm32f7x.cfg 2> /dev/null > /dev/null &
+    openocd -f $OPENOCDCONFIG 2> /dev/null > /dev/null &
 
 elif [ "$1" == "stop" ]; then
 
@@ -14,15 +18,12 @@ elif [ "$1" == "debug" ]; then
 
 elif [ "$1" == "flash" ]; then
 
-    killall openocd
-    make BOARD=OPEN7XXI_C OPENOCD_CONFIG=boards/openocd_stm32f7.cfg  deploy-openocd
+    arm-none-eabi-gdb -x boards/OPEN7XXI_C/gdbinit-flash
 
 else
 
-    #make -j4 BOARD=OPEN7XXI_C OPENOCD_CONFIG=boards/openocd_stm32f7.cfg  deploy-openocd
-    #make -j4 BOARD=OPEN7XXI_C OPENOCD_CONFIG=boards/openocd_stm32f7.cfg  $1 $2 $3
-    rm -rf build-OPEN7XXI_C
-    make -j6 BOARD=OPEN7XXI_C OPENOCD_CONFIG=boards/openocd_stm32f7.cfg DEBUG=1 V=1 USER_C_MODULES=../../lib/MicroAMP/micropython_modules $1 $2 $3
+    rm -rf build-$BOARD
+    make -j6 BOARD=$BOARD OPENOCD_CONFIG=$OPENOCDCONFIG DEBUG=1 V=1 USER_C_MODULES=../../lib/MicroAMP/micropython_modules $1 $2 $3
 
 fi
 
